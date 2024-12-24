3be31f1c84d14c0b5cfb71fab93e2b80af7608d8
novice <shirodora1248@gmail.com>
2024-12-24 15:40:26 +0900

    flowcontrol 07まで

---
+++ 03 flowcontrol/03 For is Go's "while"/for-is-gos-while.go
@@ -0,0 +1,13 @@
+// セミコロン(;)を省略することもできます。つまり、C言語などにある while は、Goでは for だけを使います。
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
---
+++ 03 flowcontrol/04 Forever/forever.go
@@ -0,0 +1,8 @@
+// ループ条件を省略すれば、無限ループ( infinite loop )になりますので、無限ループをコンパクトに表現できます。
+
+package main
+
+func main() {
+	for {
+	}
+}
